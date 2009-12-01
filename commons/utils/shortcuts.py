6c6ee6a6ac1e4d78b9206817e936e8b5ffbf7bf1
Ian Lewis <IanMLewis@gmail.com>
2009-12-01 22:09:57 +0900

    redirect_toをviews.pyに

--- commons/utils/shortcuts.py
+++ commons/utils/shortcuts.py
@@ -2,11 +2,10 @@
 from django.db import models
 from django.template import RequestContext
 from django.shortcuts import render_to_response as django_render_to_response
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseGone
-from django.utils import simplejson
-from django.utils.http import urlquote
+from django.http import HttpResponse
 from django.core.serializers.json import DjangoJSONEncoder
-from commons.utils import escapejs_json
+
+from commons.utils.javascript import simplejson, escapejs_json
 
 def make_simple_response(status=200,msg=u'処理が成功しました',extra_context=None):
     obj = {'msg':msg}
@@ -25,22 +24,3 @@ def make_simple_response403(msg=u'不正な処理です'):
 
 def make_simple_response500(msg=u'サーバー側で問題が発生しています。しばらく経ってからもう一度試してみてください'):
     return HttpResponse(escapejs_json(simplejson.dumps({'msg':msg}, cls=DjangoJSONEncoder)), status=500, mimetype='text/plain')
-
-def redirect_to(request, url, permanent=True, **kwargs):
-    """
-    A copy of the redirect_to view from django.views.generic.simple
-    Copied since redirect_to doesn't work for urls that contain non-ascii 
-    keyword arguments.
-    """
-    import re
-
-    if url is not None:
-        klass = permanent and HttpResponsePermanentRedirect or HttpResponseRedirect
-        quoted_kwargs = {}
-        for k,v in kwargs.iteritems():
-            quoted_kwargs[k] = urlquote(v)
-
-        # Encoded urls confuses python templating. Properly escape the templates.
-        return klass(urlquote(re.sub(r"%([0-9A-Z]{1,2})", r"%%\1", url) % quoted_kwargs))
-    else:
-        return HttpResponseGone()
--- commons/views.py
+++ commons/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+import re
+
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseGone
+from django.utils.http import urlquote
+
+def redirect_to(request, url, permanent=True, **kwargs):
+    """
+    A copy of the redirect_to view from django.views.generic.simple
+    Copied since redirect_to doesn't work for urls that contain non-ascii 
+    keyword arguments.
+    """
+    if url is not None:
+        klass = permanent and HttpResponsePermanentRedirect or HttpResponseRedirect
+        quoted_kwargs = {}
+        for k,v in kwargs.iteritems():
+            quoted_kwargs[k] = urlquote(v)
+
+        # Encoded urls confuses python templating. Properly escape the templates.
+        return klass(urlquote(re.sub(r"%([0-9A-Z]{1,2})", r"%%\1", url) % quoted_kwargs))
+    else:
+        return HttpResponseGone()
