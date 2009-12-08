690e5e683648274c97f0776fd30cb17270e38648
Shinya Okano <shinya.okano@beproud.jp>
2009-12-08 17:28:23 +0900

    テンプレートタグの修正

---
+++ commons/templatetags/googlead_tags.py
@@ -0,0 +1,37 @@
+# vim:fileencoding=utf-8
+
+from django.template import Node, Library, TemplateDoesNotExist, TemplateSyntaxError, Variable
+from django.template.loader import render_to_string
+
+register = Library()
+
+class GoogleAdNode(Node):
+    def __init__(self, template_name_var, block_id_var=None):
+        self.block_id_var = Variable(block_id_var) if block_id_var else None
+        self.template_name_var = Variable(template_name_var)
+
+    def render(self, context):
+        try:
+            block_id = self.block_id_var.resolve(context) if self.block_id_var else None;
+            template_name = self.template_name_var.resolve(context)
+            context_dict = {
+                "block_id": block_id if block_id else "",
+            }
+            for c in context.dicts:
+                context_dict.update(c)
+
+            return render_to_string(template_name, context_dict)
+
+        except TemplateDoesNotExist,e:
+            return ''
+
+@register.tag
+def googlead(parser, token):
+    bits = token.split_contents()
+    if len(bits) != 2 and len(bits) != 3:
+        raise TemplateSyntaxError("%r takes at least one argument." % bits[0])
+    template_name = bits[1]
+    if len(bits) == 3:
+        return GoogleAdNode(template_name, bits[2])
+    else:
+        return GoogleAdNode(template_name)
---
+++ commons/templatetags/string_tags.py
@@ -0,0 +1,52 @@
+# vim:fileencoding=utf8
+from django import template
+from time import strptime
+from django.template import Variable, TemplateSyntaxError, VariableDoesNotExist
+from django.template.defaultfilters import stringfilter
+from django.utils.html import escape
+from django.utils.encoding import force_unicode
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def abbrev(val, num=20):
+    ret = val
+    if len(val) > num:
+        ret = val[0:num] + "..."
+    return ret
+
+def cat(value, arg): 
+  """ 
+  Concatenates value with argument 
+  """ 
+  return u"%s%s" % (value, force_unicode(arg)) 
+cat.is_safe=True 
+cat = stringfilter(cat)
+register.filter(cat)
+
+class AbbrevBlockNode(template.Node):
+    """
+    A node whos output is truncated to the number of characters given.
+    """
+    def __init__(self, nodelist, max_length):
+        self.nodelist = nodelist
+        self.max_length = template.Variable(max_length)
+
+    def render(self, context):
+        try:
+            max_length = self.max_length.resolve(context)
+            text = self.nodelist.render(context)
+            return abbrev(text, max_length)
+        except template.VariableDoesNotExist:
+            return ''
+
+@register.tag(name="abbrev")
+def do_abbrev(parser, token):
+    bits = token.contents.split(' ')
+    if len(bits) < 2:
+        raise TemplateSyntaxError("'%s' takes at least one argument" % bits[0])
+    max_length = bits[1]
+    nodelist = parser.parse(('end%s' % bits[0],))
+    parser.delete_first_token()
+    return AbbrevBlockNode(nodelist, max_length)
