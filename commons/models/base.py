30180db5dbd5fced6d071d415566e30359e933f1
Ian Lewis <IanMLewis@gmail.com>
2009-12-17 10:56:07 +0900

    複数のマネジャーを使うようにした

--- commons/models/base.py
+++ commons/models/base.py
@@ -7,18 +7,31 @@ __all__ = (
 )
 
 class BaseManager(models.Manager):
-    def viewable(self):
+    def get_query_set(self):
         return self.filter(published=True)
 
     def recently_updated(self):
         return self.order_by('-utime')
 
 class BaseModel(models.Model):
+    """
+    BaseModelの実装
+    
+    使い方:
+    from django.db import models
+    from commons.models import BaseModel
+
+    class MyModel(BaseModel):
+        myfield = models.BooleanField()
+
+    MyModel.published.filter(myfield=True)
+    """
     published = models.BooleanField(u'公開フラグ', default=True, db_index=True)
     ctime = models.DateTimeField(u'作成日時', default=datetime.now, db_index=True)
     utime = models.DateTimeField(u'更新日時', auto_now=True, db_index=True) 
 
-    objects = BaseManager()
+    objects = models.Manager()
+    published = BaseManager()
 
     def remove():
         self.published = False
