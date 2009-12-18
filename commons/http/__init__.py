1cee2c069e0b85541c6c349ade219589611c1e8e
Ian Lewis <IanMLewis@gmail.com>
2009-12-18 15:11:17 +0900

    views.jsonを shortcutsにした

---
+++ commons/http/__init__.py
@@ -0,0 +1,17 @@
+# vim:fileencoding=utf-8
+
+from django.http import HttpResponse
+from django.core.serializers.json import DjangoJSONEncoder
+
+from commons.utils.javascript import simplejson
+
+__all__ = (
+    'JsonResponse',
+)
+
+class JsonResponse(HttpResponse):
+    """
+    HttpResponse descendant, which return response with ``application/json`` mimetype.
+    """
+    def __init__(self, data={}, status=200 content_type='application/json'):
+        super(JsonResponse, self).__init__(simplejson.dumps(obj, cls=DjangoJSONEncoder), content_type=content_type)
---
+++ commons/shortcuts/__init__.py
@@ -0,0 +1,46 @@
+# vim:fileencoding=utf-8
+
+from commons.http import JsonResponse
+
+__all__ = (
+        'get_object_or_None',
+        'make_simple_response',
+        'make_simple_response400',
+        'make_simple_response401',
+        'make_simple_response403',
+        'make_simple_response500',
+)
+
+def get_object_or_None(klass, *args, **kwargs):
+    """
+    Uses get() to return an object or None if the object does not exist.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the get() query.
+
+    Note: Like with get(), an MultipleObjectsReturned will be raised if more than one
+    object is found.
+    """
+    queryset = _get_queryset(klass)
+    try:
+        return queryset.get(*args, **kwargs)
+    except queryset.model.DoesNotExist:
+        return None
+
+def make_simple_response(msg=u'処理が成功しました', extra_context=None, status=200, content_type='application/json'):
+    obj = {'msg': msg}
+    if extra_context:
+        obj.update(extra_context)
+    return JsonResponse(obj, status=status, content_type=content_type)
+
+def make_simple_response400(msg=u'パラメータが不正です'):
+    return make_simple_response(msg, status=400)
+
+def make_simple_response401(msg=u'ログインしてください'):
+    return make_simple_response(msg, status=401)
+
+def make_simple_response403(msg=u'不正な処理です'):
+    return make_simple_response(msg, status=403)
+
+def make_simple_response500(msg=u'サーバー側で問題が発生しています。しばらく経ってからもう一度試してみてください'):
+    return make_simple_response(msg, status=500)
