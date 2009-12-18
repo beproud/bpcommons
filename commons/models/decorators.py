33380e9ec6b8ee5e2e3f24305f157a2b2ebeff9b
Ian Lewis <IanMLewis@gmail.com>
2009-12-18 15:10:53 +0900

    signalsデコレータを追加

---
+++ commons/models/decorators.py
@@ -0,0 +1,59 @@
+# vim:fileencoding=utf8
+
+__all__ = (
+    'signals',
+)
+
+class Signals(object):
+    '''
+    Convenient wrapper for working with Django's signals (or any other
+    implementation using same API).
+
+    Example of usage::
+
+
+       # connect to registered signal
+       @signals.post_save(sender=YourModel)
+       def sighandler(instance, **kwargs):
+           pass
+
+       # connect to any signal
+       signals.register_signal(siginstance, signame) # and then as in example above
+
+       or 
+        
+       @signals(siginstance, sender=YourModel)
+       def sighandler(instance, **kwargs):
+           pass
+
+    In any case defined function will remain as is, without any changes.
+
+    (c) 2008 Alexander Solovyov, new BSD License
+    '''
+    def __init__(self):
+        self._signals = {}
+
+        # register all Django's default signals
+        for k, v in signalmodule.__dict__.iteritems():
+            # that's hardcode, but IMHO it's better than isinstance
+            if not k.startswith('__') and k != 'Signal':
+                self.register_signal(v, k)
+
+    def __getattr__(self, name):
+        return self._connect(self._signals[name])
+
+    def __call__(self, signal, **kwargs):
+        def inner(func):
+            signal.connect(func, **kwargs)
+            return func
+        return inner
+
+    def _connect(self, signal):
+        def wrapper(**kwargs):
+            return self(signal, **kwargs)
+        return wrapper
+
+    def register_signal(self, signal, name):
+        self._signals[name] = signal
+
+signals = Signals()
