ccba3b4f09b73308e0eb9eb6426ea409b1febcac
Shinya Okano <shinya.okano@beproud.jp>
2009-12-19 15:06:57 +0900

    DotDictを追加

--- commons/utils/datastructures.py
+++ commons/utils/datastructures.py
@@ -44,3 +44,9 @@ class Bag(object):
             if s: s+=", "
             s += "%s: %s" % (x, `v`)
         return s
+
+class DotDict(dict):
+    def __getattr__(self, attr):
+        return self.get(attr, None)
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
