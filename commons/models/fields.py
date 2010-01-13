5068735d0202b12851732999da96ee1a775f81f0
Ian Lewis <IanMLewis@gmail.com>
2010-01-13 18:08:00 +0900

    PickledObjectFieldを追加

--- commons/models/fields.py
+++ commons/models/fields.py
@@ -1,9 +1,17 @@
 # vim:fileencoding=utf8
+import base64
+
+try:
+	import cPickle as pickle
+except ImportError:
+	import pickle
+
 from django.db import models
 
 __all__ = (
     'BigIntegerField',
     'PositiveBigIntegerField',
+    'PickledObjectField',
 )
 
 class BigIntegerField(models.IntegerField):
@@ -23,3 +31,15 @@ class PositiveBigIntegerField(BigIntegerField):
         defaults = {'min_value': 0}
         defaults.update(kwargs)
         return super(PositiveBigIntegerField, self).formfield(**defaults)
+
+class PickledObjectField(models.TextField):
+    __metaclass__ = models.SubfieldBase
+ 
+    def to_python(self, value):
+        if value is None: return None
+        if not isinstance(value, basestring): return value
+        return pickle.loads(base64.b64decode(value))
+ 
+    def get_db_prep_save(self, value):
+        if value is None: return
+        return base64.b64encode(pickle.dumps(value))
