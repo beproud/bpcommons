4e3cf9e5822a2dea447dcfc0d8e4fa4d9e4101b2
Ian Lewis <IanMLewis@gmail.com>
2010-01-27 20:36:28 +0900

    便利なChoicesクラスを追加

--- commons/forms/utils.py
+++ commons/forms/utils.py
@@ -1,4 +1,83 @@
 # vim:fileencoding=utf-8
+
+from django.utils.datastructures import SortedDict
+
+__all__ = (
+    'Choices',
+    'with_empty_tuple',
+    'make_choices_and_dict',
+    'make_choices_and_value',
+)
+
+class Choices(object):
+    """
+    Easy declarative "choices" tool::
+    
+        >>> STATUSES = Choices(
+        ...     ('live', 'Live'),
+        ...     ('draft', 'Draft'),
+        ...     ('hidden', 'Not Live'),
+        ... )
+        
+        # Acts like a choices list:
+        >>> list(STATUSES)
+        [(1, 'Live'), (2, 'Draft')]
+        
+        # Easily convert from code to verbose:
+        >>> STATUSES.verbose(1)
+        'Live'
+
+        >>> STATUS.prop(1)
+        'live'
+        
+        # ... and vice versa:
+        >>> STATUSES.code("draft")
+        2
+
+        status = models.SmallIntegerField(choices=STATUSES,
+        ...                                       default=STATUSES["live"])
+        
+    """
+    def __init__(self, *args, **kwargs):
+        start = kwargs.pop("start", 1)
+
+        self.code_map = SortedDict()
+        self.prop_map = SortedDict() 
+        self.reverse_map = {}
+        for code, (prop, verbose) in enumerate(args):
+            # Enumerate starts from 0, but for convention's sake we'd prefer to
+            # start choices from 1.
+            self.code_map[code+start] = verbose
+            self.prop_map[code+start] = prop
+            self.reverse_map[prop] = code+start
+            
+    def __iter__(self):
+        return self.code_map.iteritems()
+                
+    def __len__(self):
+        return len(self.code_map)
+
+    def __getitem__(self, prop):
+        return self.code(prop)
+
+    def code(self, prop):
+        """
+        Return the code version of the verbose name.
+        """
+        return self.reverse_map[prop]
+
+    def prop(self, code):
+        return self.prop_map[code]
+        
+    def verbose(self, code):
+        """
+        Return the verbose name given the code.
+        """
+        return self.code_map[code]
+    
+    def __repr__(self):
+        return repr(tuple(self.code_map.items()))
+
 def with_empty_tuple(tp, add=('', '--')):
     l = list(tp)
     l.insert(0, add)
