de0326e6b85bb2f458b9e7804c7715fabce5606c
Ian Lewis <IanMLewis@gmail.com>
2010-02-05 15:49:04 +0900

    バグ修正

--- commons/views/decorators.py
+++ commons/views/decorators.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from commons.http import JSONResponse
+from commons.http import JsonResponse
 
 def render_to(template=None):
     """
