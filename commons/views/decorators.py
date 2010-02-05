7dc020349a15727694b76e890d864bc488900bf2
Ian Lewis <IanMLewis@gmail.com>
2010-02-05 13:57:44 +0900

    django-annoyingのデコレーターを追加

---
+++ commons/views/decorators.py
@@ -0,0 +1,83 @@
+#:coding=utf8:
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from commons.http import JSONResponse
+
+def render_to(template=None):
+    """
+    Decorator for Django views that sends returned dict to render_to_response function.
+
+    Template name can be decorator parameter or TEMPLATE item in returned dictionary.
+    RequestContext always added as context instance.
+    If view doesn't return dict then decorator simply returns output.
+
+    Parameters:
+     - template: template name to use
+
+    Examples:
+    # 1. Template name in decorator parameters
+
+    @render_to('template.html')
+    def foo(request):
+        bar = Bar.object.all()  
+        return {'bar': bar}
+
+    # equals to 
+    def foo(request):
+        bar = Bar.object.all()  
+        return render_to_response('template.html', 
+                                  {'bar': bar}, 
+                                  context_instance=RequestContext(request))
+
+
+    # 2. Template name as TEMPLATE item value in return dictionary
+
+    @render_to()
+    def foo(request, category):
+        template_name = '%s.html' % category
+        return {'bar': bar, 'TEMPLATE': template_name}
+    
+    #equals to
+    def foo(request, category):
+        template_name = '%s.html' % category
+        return render_to_response(template_name, 
+                                  {'bar': bar}, 
+                                  context_instance=RequestContext(request))
+
+    Taken from django-annoying
+    """
+    def renderer(function):
+        @wraps(function)
+        def wrapper(request, *args, **kwargs):
+            output = function(request, *args, **kwargs)
+            if not isinstance(output, dict):
+                return output
+            tmpl = output.pop('TEMPLATE', template)
+            return render_to_response(tmpl, output, context_instance=RequestContext(request))
+        return wrapper
+    return renderer
+
+def ajax_request(func):
+    """
+    If view returned serializable dict, returns JsonResponse with this dict as content.
+
+    example:
+        
+        @ajax_request
+        def my_view(request):
+            news = News.objects.all()
+            news_titles = [entry.title for entry in news]
+            return {'news_titles': news_titles}
+
+    Taken from django-annoying
+    """
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        if isinstance(response, dict):
+            return JsonResponse(response)
+        else:
+            return response
+    return wrapper
