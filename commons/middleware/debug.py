f1817a16cdc28383b132b8a26cb5c5c12d0453b2
Ian Lewis <IanMLewis@gmail.com>
2010-02-12 09:57:28 +0900

    DebugMiddleware追加

---
+++ commons/middleware/debug.py
@@ -0,0 +1,11 @@
+#:coding=utf8:
+
+from django.conf import settings
+
+class DebugMiddleware(object):
+    def process_response(self, request, response):
+        if settings.DEBUG: 
+            from django.db import connection
+            for query in connection.queries:
+                print query
+        return response
