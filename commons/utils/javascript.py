4972841cd0d0e80ecc35edd7a82cbaaf74f61f69
Ian Lewis <IanMLewis@gmail.com>
2010-03-03 10:08:50 +0900

    javascript utilsを整理

--- commons/utils/javascript.py
+++ commons/utils/javascript.py
@@ -1,30 +1,15 @@
 # vim:fileencoding=utf-8
 
-from django.core.serializers.json import DjangoJSONEncoder as BaseJSONEncoder
 from django.utils.encoding import force_unicode
 from django.utils import simplejson
 
+from bputils.javascript import escapejs_json, SafeJSONEncoder
+
 __all__ = (
     'DjangoJSONEncoder',
-    'escapejs_json',
-    'force_js',
 )
 
-
-ESCAPEJS_JSON_STRING = (
-    (u'<', u'\\u003c'),
-    (u'>', u'\\u003e'),
-    (u'&', u'\\u0026'),
-)
-
-JS_CONVERT_TYPES = {
-    'bool': bool,
-    'int': int,
-    'string': str,
-    'array': list,
-}
-
-class DjangoJSONEncoder(BaseJSONEncoder):
+class DjangoJSONEncoder(SafeJSONEncoder):
     def default(self, obj):
         try:
             #datetime対応
@@ -33,24 +18,6 @@ class DjangoJSONEncoder(BaseJSONEncoder):
             # lazy翻訳オブジェクトなどの対応
             from django.utils.functional import Promise
             if isinstance(obj, Promise):
-                return force_unicode(obj)
+                return escapejs_json(force_unicode(obj))
             else:
                 raise
-
-def escapejs_json(s):
-    """
-    JSONEncoderエスケープされない文字を追加エスケープ
-    """
-    for c, code in ESCAPEJS_JSON_STRING:
-        s = s.replace(c, code)
-    return s
-
-def force_js(value, typename=None, encoder=None):
-    """
-    Changes a python value to javascript for use in templates
-    """
-    if typename:
-        typename = typename.lower()
-        if typename in JS_CONVERT_TYPES:
-            value = JS_CONVERT_TYPES[typename](value)
-    return simplejson.dumps(value, cls=(encoder or DjangoJSONEncoder))
