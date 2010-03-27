2337cf5d801408bc85a0bdd467d0cd3664266e18
Ian Lewis <IanMLewis@gmail.com>
2010-03-27 10:20:57 +0900

    cache_getのテストを追加

--- commons/tests/__init__.py
+++ commons/tests/__init__.py
@@ -1,2 +1,3 @@
 from javascript_tests import *
 from template_tests import *
+from cache_tests import *
---
+++ commons/tests/cache_tests.py
@@ -0,0 +1,22 @@
+#:coding=utf8:
+
+from django.test import TestCase as DjangoTestCase
+
+from commons.utils.cache import *
+
+class CacheGetTestCase(DjangoTestCase):
+
+    def test_cache_get(self):
+        test_val = []
+        def get_val():
+            test_val.append("test")
+            return "test-val"
+        cached_val = cache_get("test-cache-get", get_val)
+        self.assertEqual(cached_val, "test-val")
+        self.assertEqual(test_val, ["test"])
+
+        test_val = []
+        cached_val = cache_get("test-cache-get", get_val)
+        self.assertEqual(cached_val, "test-val")
+        self.assertEqual(test_val, [])
+
