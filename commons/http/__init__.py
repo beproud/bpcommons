a47da394d7b5946d052ce7c81bcc4a086c1367aa
Ian Lewis <IanMLewis@gmail.com>
2010-05-28 01:54:53 +0900

    django.shortcuts.redirect があるため、HttpResponseNamedRedirectを削除

--- commons/http/__init__.py
+++ commons/http/__init__.py
@@ -1,15 +1,12 @@
 # vim:fileencoding=utf-8
 
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.utils import simplejson
-from django.utils.encoding import iri_to_uri
 
 from commons.utils.javascript import DjangoJSONEncoder
+
 __all__ = (
     'JSONResponse',
-    'HttpResponseNamedRedirect',
-    'HttpResponseNamedPermanentRedirect',
 )
 
 class JSONResponse(HttpResponse):
@@ -19,10 +16,3 @@ class JSONResponse(HttpResponse):
     def __init__(self, data={}, status=200, content_type='application/json'):
         super(JSONResponse, self).__init__(simplejson.dumps(data, cls=DjangoJSONEncoder), status=status, content_type=content_type)
 
-class HttpResponseNamedRedirect(HttpResponseRedirect):
-    def __init__(self, lookup_view, *args, **kwargs):
-        super(HttpResponseNamedRedirect, self).__init__(reverse(lookup_view, *args, **kwargs))
-
-class HttpResponseNamedPermanentRedirect(HttpResponsePermanentRedirect):
-    def __init__(self, lookup_view, *args, **kwargs):
-        super(HttpResponseNamedPermanentRedirect, self).__init__(reverse(lookup_view, *args, **kwargs))
