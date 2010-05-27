d159ed26370f884f2e5a362c6c7ed88878887450
Ian Lewis <IanMLewis@gmail.com>
2010-05-28 03:17:03 +0900

    remove() と unremove() のテストを追加

--- commons/tests/models/base/models.py
+++ commons/tests/models/base/models.py
@@ -36,3 +36,22 @@ class BaseModelTest(DjangoTestCase):
             self.assertTrue(not obj.del_flg)
             previous = obj
 
+    def test_remove(self):
+        obj = TestBaseModel.objects.get(pk=1)
+        self.assertTrue(not obj.del_flg)
+
+        obj.remove()
+        self.assertTrue(obj.del_flg)
+
+        obj_updated = TestBaseModel.objects.get(pk=1)
+        self.assertTrue(obj_updated.del_flg)
+
+    def test_unremove(self):
+        obj = TestBaseModel.objects.get(pk=3)
+        self.assertTrue(obj.del_flg)
+
+        obj.unremove()
+        self.assertTrue(not obj.del_flg)
+
+        obj_updated = TestBaseModel.objects.get(pk=3)
+        self.assertTrue(not obj_updated.del_flg)
