84659254b5b1f9776a4d4806cae8f711b0096725
Ian Lewis <IanMLewis@gmail.com>
2010-05-28 01:52:22 +0900

    セキュリティ問題のため、HttpResponseReloadを削除

--- commons/http/__init__.py
+++ commons/http/__init__.py
@@ -8,7 +8,6 @@ from django.utils.encoding import iri_to_uri
 from commons.utils.javascript import DjangoJSONEncoder
 __all__ = (
     'JSONResponse',
-    'HttpResponseReload',
     'HttpResponseNamedRedirect',
     'HttpResponseNamedPermanentRedirect',
 )
@@ -19,29 +18,6 @@ class JSONResponse(HttpResponse):
     """
     def __init__(self, data={}, status=200, content_type='application/json'):
         super(JSONResponse, self).__init__(simplejson.dumps(data, cls=DjangoJSONEncoder), status=status, content_type=content_type)
-
-class HttpResponseReload(HttpResponse):
-    """
-    Reload page and stay on the same page from where request was made.
-
-    example:
-
-    def simple_view(request):
-        if request.POST:
-            form = CommentForm(request.POST):
-            if form.is_valid():
-                form.save()
-                return HttpResponseReload(request)
-        else:
-            form = CommentForm()
-        return render_to_response('some_template.html', {'form': form})
-    """
-    status_code = 302
-
-    def __init__(self, request):
-        HttpResponse.__init__(self)
-        referer = request.META.get('HTTP_REFERER')
-        self['Location'] = iri_to_uri(referer or "/")
 
 class HttpResponseNamedRedirect(HttpResponseRedirect):
     def __init__(self, lookup_view, *args, **kwargs):
