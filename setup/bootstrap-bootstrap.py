39c8e5b78489e0220f3bfcecfd9761de9cc06a89
Ian Lewis <IanMLewis@gmail.com>
2010-06-29 15:46:25 +0900

    buildbot 環境修正

--- setup/bootstrap-bootstrap.py
+++ setup/bootstrap-bootstrap.py
@@ -3,20 +3,11 @@ output = virtualenv.create_bootstrap_script(textwrap.dedent("""
 import os, subprocess
 
 def extend_parser(optparse_parser):
-    optparse_parser.add_option("-b", "--buildbot", dest="buildbot", action="store_true",
-        default=False, help="Bootstrap a buildbot build environment.")
-    optparse_parser.add_option("-a", "--all-requirements", dest="allreqs", action="store_true",
-        default=False, help="Install all requirements.")
+    optparse_parser.add_option("-r", "--requirement", dest="req_file",
+        default=os.path.join(os.path.dirname(__file__), "requirements.txt"),
+        help="Bootstrap a buildbot build environment.")
 
 def after_install(options, home_dir):
-    proj_path = os.path.dirname(__file__)
-    if options.buildbot:
-        req_file = "requirements_buildbot.txt"
-    elif options.allreqs:
-        req_file = "requirements_all.txt"
-    else:
-        req_file = "requirements.txt" 
-    subprocess.call(["pip", "install", "-E", home_dir, "--requirement", 
-        os.path.join(proj_path, req_file)])
+    subprocess.call(["pip", "install", "-E", home_dir, "--requirement", options.req_file])
 """))
 f = open(os.path.join('setup', 'bootstrap.py'), 'w').write(output)
