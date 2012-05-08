1763923fd42875e647f2e231c65b21a012ce3b69
Ian Lewis <IanMLewis@gmail.com>
2012-05-08 15:37:25 +0900

    Southのルールを追加 refs #8191
    --HG--
    branch : t8191

--- beproud/django/commons/models/fields.py
+++ beproud/django/commons/models/fields.py
@@ -98,7 +98,6 @@ else:
     """
     For Django 1.1
     """
-    from django.utils.translation import ugettext_lazy as _
 
     class BigIntegerField(models.IntegerField):
         empty_strings_allowed = False
@@ -227,3 +226,15 @@ class JSONField(models.TextField):
     def value_to_string(self, obj):
         value = self._get_val_from_obj(obj)
         return self.get_db_prep_value(value)
+
+
+try:
+    from south.modelsinspector import add_introspection_rules
+
+    add_introspection_rules([
+
+        ((BigAutoField,), [], {},),
+
+    ], [r"^beproud\.django\.commons\.models"])
+except ImportError:
+    pass
