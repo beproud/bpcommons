810b50de8332b3d9351347defa43deae14b7009a
Ian Lewis <IanMLewis@gmail.com>
2012-05-08 13:28:39 +0900

    0.2 リリース

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
  
 setup (
     name='beproud.django.commons',
-    version='0.1',
+    version='0.2',
     description='Common utilities for Django',
     author='K.K. BeProud',
     author_email='ianmlewis@beproud.jp',
